5a1c252220bc626eeb1d7077452c0805a0bbf0ba
barek <bareketatia@gmail.com>
2024-02-21 19:34:00 +0200

    add checkboxes for the cookie

--- src/Components/CookieDesign.js
+++ src/Components/CookieDesign.js
@@ -1,12 +1,40 @@
 import React, {useEffect} from 'react';
-
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import '../Styles/LoginStyle.scss';
+import '../Styles/CookieDesign.css'
 const CookieDesign = () => {
-
 
     return (
         <div>
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    <Circle x={window.innerWidth/2} y={window.innerHeight/2} radius={200} fill="#E6BF83" />
+                </Layer>
+            </Stage>
+            <button className={"modal-button"} style={{position: 'fixed', bottom: '50px', right: '10px'}}>Submit</button>
 
-        </div>
+            <label style={{ bottom: '300px' }}>
+
+                <input type="checkbox" style={{marginRight:"10px" }} />
+                Milk Chocolate
+            </label>
+            <label style={{  bottom: '350px'}}>
+
+                <input type="checkbox" style={{marginRight:"10px" }} />
+                White Chocolate
+            </label>
+            <label style={{bottom: '400px' }}>
+
+                <input type="checkbox" style={{marginRight:"10px" }} />
+                 Salt Caramel
+            </label>
+            <label style={{ bottom: '450px' }}>
+
+                <input type="checkbox" style={{marginRight:"10px" }} />
+                Brown Cookie Dough
+            </label>
+
+                </div>
     );
 };
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Cookies from 'js-cookie';
 import UserMenu from "./UserMenu";
 import '../Styles/LoginStyle.scss';
 import CookieDesign from "./CookieDesign";
+import {Circle, Layer, Stage} from "react-konva";
 
 
 const Home = () => {
     const username =Cookies.get("username");
 const [isPressed,setIsPressed]=useState(false);
+ useEffect(()=>{
+
+ },[isPressed])
+
     return (
         <div>
          <UserMenu/>
         <h1>Welcome {username}</h1>
             <div className={"button-container"}>
-                <button onClick={()=>{setIsPressed(true)}} className="modal-button">set your dream cookie</button>
-                {/*{*/}
-                {/*    isPressed&&*/}
-                {/*    <CookieDesign/>*/}
-                {/*}*/}
+
+                <button onClick={()=>{setIsPressed(!isPressed)}}  style={ { visibility: isPressed ? 'hidden' : 'visible' } } className="modal-button">set your dream cookie</button>
+                {
+                    isPressed&&
+                        <CookieDesign/>
+
+
+
+
+                }
 
             </div>
 
